fc648b99dde5899980ed21f2d919f151ced5ec88
Sebastian <manzaseb@grinnell.edu>
2024-12-10 16:45:06 -0600

    Initializations

---
+++ src/main/java/engine/TARS.java
@@ -0,0 +1,11 @@
+package engine;
+
+/**
+ * The main class of the engine. Will prompt the user, initialize the MCT, etc.
+ * @author Sebastian Manza
+ */
+public class TARS {
+    public static void main(String[] args) {
+        // STUB
+    } // main(String[])
+} // TARS
